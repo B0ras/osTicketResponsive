74f7ce28ea7e4b9e5c4d5fe1de5934f4d0952d38
NasosBoras <borasnasos@gmail.com>
2022-12-01 21:19:05 +0200

    Completed install.php

--- setup/inc/header.inc.php
+++ setup/inc/header.inc.php
@@ -1,56 +1,65 @@
 <?php
 if ($cfg)
-    header("Content-Security-Policy: frame-ancestors ".$cfg->getAllowIframes().";");
+    header("Content-Security-Policy: frame-ancestors " . $cfg->getAllowIframes() . ";");
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-    "http://www.w3.org/TR/html4/loose.dtd">
-<html <?php
-if (($lang = Internationalization::getCurrentLanguage())
-        && ($info = Internationalization::getLanguageInfo($lang))
-        && (@$info['direction'] == 'rtl'))
-    echo 'dir="rtl" class="rtl"';
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html <?php if (
+    ($lang=Internationalization::getCurrentLanguage()) &&
+    ($info=Internationalization::getLanguageInfo($lang)) && (@$info['direction']=='rtl')
+) echo 'dir="rtl" class="rtl"';
+    // Dropped IE Support Warning if (osTicket::is_ie()) $warning=__('osTicket no longer supports Internet Explorer.');
+    ?>>
 
-// Dropped IE Support Warning
-if (osTicket::is_ie())
-    $warning = __('osTicket no longer supports Internet Explorer.');
-?>>
 <head>
-    <title><?php echo $wizard['title']; ?></title>
+    <title>
+        <?php echo $wizard['title']; ?>
+    </title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" href="css/wizard.css">
+    <!-- <link rel="stylesheet" href="css/wizard.css"> -->
+    <link type="text/css" rel="stylesheet" href="css/wizard_responsive.css">
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/flags.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"
+        async></script>
 </head>
-<body>
-    <div id="wizard" class="container text-wrap">
-        <?php if ($warning) echo sprintf('<div class="warning_bar">%s</div>', $warning); ?>
+
+<body class="">
+    <div id="wizard" class="container-fluid col-sm1">
+        <?php if ($warning)
+            echo sprintf('<div class="warning_bar">%s</div>', $warning); ?>
         <div id="header" class="row">
             <img id="logo" src="./images/<?php echo $wizard['logo'] ?: 'logo.png'; ?>" alt="osTicket">
-            <div class="info"><?php echo $wizard['tagline']; ?></div>
-            <br/>
+            <div class="info">
+                <?php echo $wizard['tagline']; ?>
+            </div>
+            <br />
             <ul class="links">
                 <li>
-                   <?php
-                   foreach($wizard['menu'] as $k=>$v)
-                    echo sprintf('<a target="_blank" href="%s">%s</a> &mdash; ',$v,$k);
-                   ?>
-                    <a target="_blank" href="https://osticket.com/contact-us"><?php echo __('Contact Us');?></a>
+                    <?php
+                    foreach ($wizard['menu'] as $k => $v)
+                        echo sprintf('<a target="_blank" href="%s">%s</a> &mdash; ', $v, $k);
+                    ?>
+                    <a target="_blank" href="https://osticket.com/contact-us">
+                        <?php echo __('Contact Us'); ?>
+                    </a>
                 </li>
             </ul>
             <div class="flags">
-<?php
-if (($all_langs = Internationalization::availableLanguages())
-    && (count($all_langs) > 1)
-) {
-    foreach ($all_langs as $code=>$info) {
-        list($lang, $locale) = explode('_', $code);
-?>
-        <a class="flag flag-<?php echo strtolower($locale ?: $info['flag'] ?: $lang); ?>"
-            href="?<?php echo urlencode($_GET['QUERY_STRING']); ?>&amp;lang=<?php echo $code;
-            ?>" title="<?php echo Internationalization::getLanguageDescription($code); ?>">&nbsp;</a>
-<?php }
-} ?>
+                <?php
+                if (
+                    ($all_langs = Internationalization::availableLanguages())
+                    && (count($all_langs) > 1)
+                ) {
+                    foreach ($all_langs as $code => $info) {
+                        list($lang, $locale) = explode('_', $code);
+                ?>
+                <a class="flag flag-<?php echo strtolower($locale ?: $info['flag'] ?: $lang); ?>" href="?<?php echo urlencode($_GET['QUERY_STRING']); ?>&amp;lang=<?php echo $code;
+                                 ?>"
+                    title="<?php echo Internationalization::getLanguageDescription($code); ?>">&nbsp;</a>
+                <?php }
+                } ?>
             </div>
         </div>
         <div class="clear"></div>
--- setup/inc/install-prereq.inc.php
+++ setup/inc/install-prereq.inc.php
@@ -2,13 +2,17 @@
 if (!defined('SETUPINC'))
     die('Kwaheri!');
 
+// Icons
+$yes = "bi-check-square yes";
+$no = "bi-x-square no";
+
 ?>
 
-<div id="main" class="row">
+<div id="main" class="container col-12">
     <h2 class="row text-start">
         <?php echo __('Thank You for Choosing osTicket!'); ?>
     </h2>
-    <div id="intro" class="row container">
+    <div id="intro" class="container">
         <p class="row text-wrap ">
             <?php echo __('We are delighted you have chosen osTicket for your customer support ticketing system!'); ?>
         </p>
@@ -19,7 +23,7 @@ if (!defined('SETUPINC'))
     <h2 class="row text-start">
         <?php echo __('Prerequisites'); ?>
     </h2>
-    <p class="row text-start mr-5">
+    <p class="col-11 text-start mr-5">
         <?php echo __("Before we begin, we'll check your server configuration to make sure you meet the minimum requirements to run the latest version of osTicket."); ?>
     </p>
     <h3>
@@ -28,14 +32,16 @@ if (!defined('SETUPINC'))
         </font>
     </h3>
     <?php echo __('These items are necessary in order to install and use osTicket.'); ?>
-    <ul class="progress">
-        <li class="<?php echo $installer->check_php() ? 'yes' : 'no'; ?>">
+    <ul class="progress row col-12 bi">
+        <li>
+            <i class="<?php echo $installer->check_php() ? $yes : $no; ?>"></i>
             <?php echo sprintf(__('%s or greater'), '<span class="ltr">PHP v' . SetupWizard::getPHPVersion() . '</span>'); ?>
             &mdash; <small class="ltr">(<b>
                     <?php echo PHP_VERSION; ?>
                 </b>)</small>
         </li>
-        <li class="<?php echo $installer->check_mysql() ? 'yes' : 'no'; ?>">
+        <li>
+            <i class="<?php echo $installer->check_mysql() ? $yes : $no; ?>"></i>
             <?php echo __('MySQLi extension for PHP'); ?> &mdash; <small><b>
                     <?php
                     echo extension_loaded('mysqli') ? __('module loaded') : __('missing!'); ?>
@@ -45,73 +51,121 @@ if (!defined('SETUPINC'))
     <h3>
         <?php echo __('Recommended'); ?>:
     </h3>
-    <?php echo __('You can use osTicket without these, but you may not be able to use all features.'); ?>
-    <ul class="progress">
-        <li class="<?php echo extension_loaded('gd') ? 'yes' : 'no'; ?>">Gdlib
+    <div class="col-12">
+        <?php echo __('You can use osTicket without these, but you may not be able to use all features.'); ?>
+    </div>
+    <ul class="progress row col-12">
+        <li>
+            <i class="<?php echo extension_loaded('gd') ? $yes : $no; ?>"></i>
+            Gdlib
             <?php echo __('Extension'); ?>
         </li>
-        <li class="<?php echo extension_loaded('imap') ? 'yes' : 'no'; ?>">PHP IMAP
+        <li>
+            <i class="<?php echo extension_loaded('imap') ? $yes : $no; ?>"></i>
+            PHP IMAP
             <?php echo __('Extension'); ?> &mdash; <em>
                 <?php
                 echo __('Required for mail fetching'); ?>
             </em>
         </li>
-        <li class="<?php echo extension_loaded('xml') ? 'yes' : 'no'; ?>">PHP XML
+        <li>
+            <i class=" <?php echo extension_loaded('xml') ? $yes : $no; ?>"></i>
+            PHP XML
             <?php echo __('Extension'); ?>
             <?php
             echo __('(for XML API)'); ?>
         </li>
-        <li class="<?php echo extension_loaded('dom') ? 'yes' : 'no'; ?>">PHP XML-DOM
+        <li>
+            <i class="<?php echo extension_loaded('dom') ? $yes : $no; ?>"></i>
+            PHP XML-DOM
             <?php echo __('Extension'); ?>
             <?php
             echo __('(for HTML email processing)'); ?>
         </li>
-        <li class="<?php echo extension_loaded('json') ? 'yes' : 'no'; ?>">PHP JSON
+        <li>
+            <i class="<?php echo extension_loaded('json') ? $yes : $no; ?>"></i>
+            PHP JSON
             <?php echo __('Extension'); ?>
             <?php
             echo __('(faster performance)'); ?>
         </li>
-        <li class="<?php echo extension_loaded('mbstring') ? 'yes' : 'no'; ?>">Mbstring
+        <li>
+            <i class="<?php echo extension_loaded('mbstring') ? $yes : $no; ?>"></i>
+            Mbstring
             <?php echo __('Extension'); ?> &mdash;
             <?php
             echo __('recommended for all installations'); ?>
         </li>
-        <li class="<?php echo extension_loaded('phar') ? 'yes' : 'no'; ?>">Phar
+        <li>
+            <i class="<?php echo extension_loaded('phar') ? $yes : $no; ?>"></i>
+            Phar
             <?php echo __('Extension'); ?> &mdash;
             <?php
             echo __('recommended for plugins and language packs'); ?>
         </li>
-        <li class="<?php echo extension_loaded('intl') ? 'yes' : 'no'; ?>">Intl
+        <li>
+            <i class="<?php echo extension_loaded('intl') ? $yes : $no; ?>"></i>
+            Intl
             <?php echo __('Extension'); ?> &mdash;
             <?php
             echo __('recommended for improved localization'); ?>
         </li>
-        <li class="<?php echo extension_loaded('apcu') ? 'yes' : 'no'; ?>">APCu
+        <li>
+            <i class="<?php echo extension_loaded('apcu') ? $yes : $no; ?>"></i>
+            APCu
             <?php echo __('Extension'); ?> &mdash;
             <?php
             echo __('(faster performance)'); ?>
         </li>
-        <li class="<?php echo extension_loaded('Zend OPcache') ? 'yes' : 'no'; ?>">Zend OPcache
+        <li>
+            <i class="<?php echo extension_loaded('Zend OPcache') ? $yes : $no; ?>"></i>
+            Zend OPcache
             <?php echo __('Extension'); ?> &mdash;
             <?php
             echo __('(faster performance)'); ?>
         </li>
     </ul>
+    <div id="sidebar" class="col-12 accordion">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#helpDescription" aria-expanded="false" aria-controls="helpDescription">
+                    <h3 class="row align-items-center">
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="128" height="32" fill="currentColor"
+                            class="bi bi-gear-wide col-2 align-self-center" viewBox="0 0 16 16">
+                            <path
+                                d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z" />
+                        </svg> -->
+                        <i class="bi bi-gear-wide col-2 text-center">
+
+                        </i>
+                        <div class="col-10 lh-1 help">
+                            <?php echo __('Need Help?'); ?>
+                        </div>
+                    </h3>
+                </button>
+            </h2>
+            <div class="accordion-collapse collapse" aria-labelledby="helpDescription" data-bs-parent="#sidebar"
+                id="helpDescription">
+                <div class="accordion-body">
+                    <p>
+                        <?php echo __('If you are looking for a greater level of support, we provide <u>professional installation services</u> and commercial support with guaranteed response times, and access to the core development team. We can also help customize osTicket or even add new features to the system to meet your unique needs.'); ?>
+                        <a target="_blank" href="https://osticket.com/support">
+                            <?php echo __('Learn More!'); ?>
+                        </a>
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
     <div id="bar">
         <form method="post" action="install.php">
             <input type="hidden" name="s" value="prereq">
-            <input class="btn" type="submit" name="submit" value="<?php echo __('Continue'); ?> &raquo;">
+            <button type="submit" class="btn btn-primary">
+                <?php echo __('Continue'); ?> &raquo;
+            </button>
+            <!-- <input class="btn-primary" type="submit" name="submit"> -->
         </form>
     </div>
-</div>
-<div id="sidebar">
-    <h3>
-        <?php echo __('Need Help?'); ?>
-    </h3>
-    <p>
-        <?php echo __('If you are looking for a greater level of support, we provide <u>professional installation services</u> and commercial support with guaranteed response times, and access to the core development team. We can also help customize osTicket or even add new features to the system to meet your unique needs.'); ?>
-        <a target="_blank" href="https://osticket.com/support">
-            <?php echo __('Learn More!'); ?>
-        </a>
-    </p>
 </div>
