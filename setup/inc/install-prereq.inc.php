b58c971f16bc84d1706976b89378da8e178f199c
NasosBoras <borasnasos@gmail.com>
2022-12-12 20:23:05 +0200

    Turned need help element to .inc.php

--- setup/inc/install-prereq.inc.php
+++ setup/inc/install-prereq.inc.php
@@ -126,33 +126,7 @@ $no = "bi-x-square no";
         </li>
     </ul>
     <div id="sidebar" class="col-12 accordion">
-        <div class="accordion-item">
-            <h2 class="accordion-header">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#helpDescription" aria-expanded="false" aria-controls="helpDescription">
-                    <h3 class="row no-decoration">
-                        <i class="bi bi-gear-wide col-2 text-center">
-
-                        </i>
-                        <div class="col-10 lh-1 help">
-                            <?php echo __('Need Help?'); ?>
-                        </div>
-                    </h3>
-                </button>
-            </h2>
-            <div class="accordion-collapse collapse" aria-labelledby="helpDescription" data-bs-parent="#sidebar"
-                id="helpDescription">
-                <div class="accordion-body">
-                    <p>
-                        <?php echo __('If you are looking for a greater level of support, we provide <u>professional installation services</u> and commercial support with guaranteed response times, and access to the core development team. We can also help customize osTicket or even add new features to the system to meet your unique needs.'); ?>
-                        <a target="_blank" href="https://osticket.com/support">
-                            <?php echo __('Learn More!'); ?>
-                        </a>
-                    </p>
-
-                </div>
-            </div>
-        </div>
+        <?php require(INC_DIR . 'help-accordion.inc.php'); ?>
     </div>
     <div id="bar">
         <form method="post" action="install.php">
