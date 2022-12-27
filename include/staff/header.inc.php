da8885c87ee7f13ff2c7a5b509aea79b945df3b0
NasosBoras <borasnasos@gmail.com>
2022-12-27 16:38:38 +0200

    Made Sub Bar Responsive.

--- include/staff/header.inc.php
+++ include/staff/header.inc.php
@@ -1,32 +1,38 @@
 <?php
 header("Content-Type: text/html; charset=UTF-8");
-header("Content-Security-Policy: frame-ancestors ".$cfg->getAllowIframes().";");
+header("Content-Security-Policy: frame-ancestors " . $cfg->getAllowIframes() . ";");
 
-$title = ($ost && ($title=$ost->getPageTitle()))
-    ? $title : ('osTicket :: '.__('Staff Control Panel'));
+$title = ($ost && ($title = $ost->getPageTitle()))
+    ? $title : ('osTicket :: ' . __('Staff Control Panel'));
 
 if (!isset($_SERVER['HTTP_X_PJAX'])) { ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html<?php
-if (($lang = Internationalization::getCurrentLanguage())
-        && ($info = Internationalization::getLanguageInfo($lang))
-        && (@$info['direction'] == 'rtl'))
-    echo ' dir="rtl" class="rtl"';
-if ($lang) {
-    echo ' lang="' . Internationalization::rfc1766($lang) . '"';
-}
-
-// Dropped IE Support Warning
-if (osTicket::is_ie())
-    $ost->setWarning(__('osTicket no longer supports Internet Explorer.'));
+<html <?php
+    if (
+        ($lang = Internationalization::getCurrentLanguage()) &&
+        ($info = Internationalization::getLanguageInfo($lang)) && (@$info['direction'] == 'rtl')
+    )
+        echo ' dir="rtl"
+    class="rtl"';
+    if ($lang) {
+        echo ' lang="' . Internationalization::rfc1766($lang) . '"';
+    }
+    // Dropped IE Support Warning 
+    if (osTicket::is_ie()) {
+        $ost->setWarning(__('osTicket no longer supports Internet Explorer.'));
+    }
 ?>>
+
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta http-equiv="cache-control" content="no-cache" />
     <meta http-equiv="pragma" content="no-cache" />
     <meta http-equiv="x-pjax-version" content="<?php echo GIT_VERSION; ?>">
-    <title><?php echo Format::htmlchars($title); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+        <?php echo Format::htmlchars($title); ?>
+    </title>
     <!--[if IE]>
     <style type="text/css">
         .tip_shadow { display:block !important; }
@@ -34,86 +40,117 @@ if (osTicket::is_ie())
     <![endif]-->
     <script type="text/javascript" src="<?php echo ROOT_PATH; ?>js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/thread.css" media="all">
-    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/scp.css" media="all">
+    <!-- <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/scp.css" media="all"> -->
+    <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/scp_responsive.css" media="all">
     <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/redactor.css" media="screen">
     <link rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/typeahead.css" media="screen">
     <link type="text/css" href="<?php echo ROOT_PATH; ?>css/ui-lightness/jquery-ui-1.13.1.custom.min.css"
-         rel="stylesheet" media="screen" />
+        rel="stylesheet" media="screen" />
     <link rel="stylesheet" href="<?php echo ROOT_PATH ?>css/jquery-ui-timepicker-addon.css" media="all">
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome.min.css">
     <!--[if IE 7]>
     <link rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/font-awesome-ie7.min.css">
     <![endif]-->
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/dropdown.css">
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/loadingbar.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/loadingbar.css" />
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/flags.css">
     <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/select2.min.css">
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/rtl.css"/>
-    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/translatable.css"/>
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH; ?>css/rtl.css" />
+    <link type="text/css" rel="stylesheet" href="<?php echo ROOT_PATH ?>scp/css/translatable.css" />
     <!-- Favicons -->
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="<?php echo ROOT_PATH ?>images/oscar-favicon-16x16.png" sizes="16x16" />
-
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"></script>
     <?php
-    if($ost && ($headers=$ost->getExtraHeaders())) {
-        echo "\n\t".implode("\n\t", $headers)."\n";
+    if ($ost && ($headers = $ost->getExtraHeaders())) {
+        echo "\n\t" . implode("\n\t", $headers) . "\n";
     }
     ?>
 </head>
+
 <body>
-<div id="container">
-    <?php
-    if($ost->getError())
+    <div id="container" class="">
+        <?php
+    if ($ost->getError())
         echo sprintf('<div id="error_bar">%s</div>', $ost->getError());
-    elseif($ost->getWarning())
+    elseif ($ost->getWarning())
         echo sprintf('<div id="warning_bar">%s</div>', $ost->getWarning());
-    elseif($ost->getNotice())
+    elseif ($ost->getNotice())
         echo sprintf('<div id="notice_bar">%s</div>', $ost->getNotice());
-    ?>
-    <div id="header">
-        <p id="info" class="pull-right no-pjax"><?php echo sprintf(__('Welcome, %s.'), '<strong>'.$thisstaff->getFirstName().'</strong>'); ?>
-           <?php
-            if($thisstaff->isAdmin() && !defined('ADMINPAGE')) { ?>
-            | <a href="<?php echo ROOT_PATH ?>scp/admin.php" class="no-pjax"><?php echo __('Admin Panel'); ?></a>
-            <?php }else{ ?>
-            | <a href="<?php echo ROOT_PATH ?>scp/index.php" class="no-pjax"><?php echo __('Agent Panel'); ?></a>
-            <?php } ?>
-            | <a href="<?php echo ROOT_PATH ?>scp/profile.php"><?php echo __('Profile'); ?></a>
-            | <a href="<?php echo ROOT_PATH ?>scp/logout.php?auth=<?php echo $ost->getLinkToken(); ?>" class="no-pjax"><?php echo __('Log Out'); ?></a>
-        </p>
-        <a href="<?php echo ROOT_PATH ?>scp/index.php" class="no-pjax" id="logo">
-            <span class="valign-helper"></span>
-            <img src="<?php echo ROOT_PATH ?>scp/logo.php?<?php echo strtotime($cfg->lastModified('staff_logo_id')); ?>" alt="osTicket &mdash; <?php echo __('Customer Support System'); ?>"/>
-        </a>
-    </div>
-    <div id="pjax-container" class="<?php if ($_POST) echo 'no-pjax'; ?>">
-<?php } else {
+        ?>
+        <div id="header" class="row">
+            <div class="col-12 col-md-6 order-2">
+                <p id="info" class="pull-right no-pjax ">
+                    <?php echo sprintf(__('Welcome, %s.'), '<strong>' . $thisstaff->getFirstName() . '</strong>'); ?>
+                    <?php
+    if ($thisstaff->isAdmin() && !defined('ADMINPAGE')) { ?>
+                    | <a href="<?php echo ROOT_PATH ?>scp/admin.php" class="no-pjax">
+                        <?php echo __('Admin Panel'); ?>
+                    </a>
+                    <?php } else { ?>
+                    | <a href="<?php echo ROOT_PATH ?>scp/index.php" class="no-pjax">
+                        <?php echo __('Agent Panel'); ?>
+                    </a>
+                    <?php } ?>
+                    | <a href="<?php echo ROOT_PATH ?>scp/profile.php">
+                        <?php echo __('Profile'); ?>
+                    </a>
+                    | <a href="<?php echo ROOT_PATH ?>scp/logout.php?auth=<?php echo $ost->getLinkToken(); ?>"
+                        class="no-pjax">
+                        <?php echo __('Log Out'); ?>
+                    </a>
+                </p>
+            </div>
+
+            <a href="<?php echo ROOT_PATH ?>scp/index.php" class="no-pjax order-1 col-12 col-md-6" id="logo">
+                <span class="valign-helper"></span>
+                <img src="<?php echo ROOT_PATH ?>scp/logo.php?<?php echo strtotime($cfg->lastModified('staff_logo_id')); ?>"
+                    alt="osTicket &mdash; <?php echo __('Customer Support System'); ?>" />
+            </a>
+        </div>
+        <div id="pjax-container" class="row <?php if ($_POST)
+        echo 'no-pjax'; ?>">
+            <?php } else {
     header('X-PJAX-Version: ' . GIT_VERSION);
     if ($pjax = $ost->getExtraPjax()) { ?>
-    <script type="text/javascript">
-    <?php foreach (array_filter($pjax) as $s) echo $s.";"; ?>
-    </script>
-    <?php }
-    foreach ($ost->getExtraHeaders() as $h) {
+            <script type="text/javascript">
+    <?php foreach (array_filter($pjax) as $s)
+            echo $s . ";"; ?>
+            </script>
+            <?php }foreach ($ost->getExtraHeaders() as $h) {
         if (strpos($h, '<script ') !== false)
             echo $h;
     } ?>
-    <title><?php echo ($ost && ($title=$ost->getPageTitle()))?$title:'osTicket :: '.__('Staff Control Panel'); ?></title><?php
+            <title>
+                <?php echo ($ost && ($title = $ost->getPageTitle())) ? $title : 'osTicket :: ' . __('Staff Control Panel'); ?>
+            </title>
+            <?php
 } # endif X_PJAX ?>
-    <ul id="nav">
-<?php include STAFFINC_DIR . "templates/navigation.tmpl.php"; ?>
-    </ul>
-    <?php include STAFFINC_DIR . "templates/sub-navigation.tmpl.php"; ?>
+            <ul id="nav" class="nav nav-tabs">
+                <?php include STAFFINC_DIR . "templates/navigation.tmpl.php"; ?>
+            </ul>
+            <?php include STAFFINC_DIR . "templates/sub-navigation.tmpl.php"; ?>
 
-        <div id="content">
-        <?php if(isset($errors['err'])) { ?>
-            <div id="msg_error"><?php echo $errors['err']; ?></div>
-        <?php }elseif($msg) { ?>
-            <div id="msg_notice"><?php echo $msg; ?></div>
-        <?php }elseif($warn) { ?>
-            <div id="msg_warning"><?php echo $warn; ?></div>
-        <?php }
-        foreach (Messages::getMessages() as $M) { ?>
-            <div class="<?php echo strtolower($M->getLevel()); ?>-banner"><?php
-                echo (string) $M; ?></div>
-<?php   } ?>
+            <div id="content">
+                <?php if (isset($errors['err'])) { ?>
+                <div id="msg_error">
+                    <?php echo $errors['err']; ?>
+                </div>
+                <?php } elseif ($msg) { ?>
+                <div id="msg_notice">
+                    <?php echo $msg; ?>
+                </div>
+                <?php } elseif ($warn) { ?>
+                <div id="msg_warning">
+                    <?php echo $warn; ?>
+                </div>
+                <?php }
+                foreach (Messages::getMessages() as $M) { ?>
+                <div class="<?php echo strtolower($M->getLevel()); ?>-banner">
+                    <?php
+                    echo (string) $M; ?>
+                </div>
+                <?php } ?>
--- include/staff/templates/navigation.tmpl.php
+++ include/staff/templates/navigation.tmpl.php
@@ -1,17 +1,19 @@
 <?php
-if($nav && ($tabs=$nav->getTabs()) && is_array($tabs)){
-    foreach($tabs as $name =>$tab) {
+if ($nav && ($tabs = $nav->getTabs()) && is_array($tabs)) {
+    foreach ($tabs as $name => $tab) {
         if ($tab['href'][0] != '/')
             $tab['href'] = ROOT_PATH . 'scp/' . $tab['href'];
-        echo sprintf('<li class="%s %s"><a href="%s">%s</a>',
-            isset($tab['active']) ? 'active':'inactive',
+        echo sprintf(
+            '<li class="%s %s nav-item"><a href="%s" class="nav-link">%s</a>',
+            isset($tab['active']) ? 'active' : 'inactive',
             @$tab['class'] ?: '',
-            $tab['href'],$tab['desc']);
-        if(!isset($tab['active']) && ($subnav=$nav->getSubMenu($name))){
+            $tab['href'], $tab['desc']
+        );
+        if (!isset($tab['active']) && ($subnav = $nav->getSubMenu($name))) {
             echo "<ul>\n";
-            foreach($subnav as $k => $item) {
-                if (isset($item['id']) && !($id=$item['id']))
-                    $id="nav$k";
+            foreach ($subnav as $k => $item) {
+                if (isset($item['id']) && !($id = $item['id']))
+                    $id = "nav$k";
                 if ($item['href'][0] != '/')
                     $item['href'] = ROOT_PATH . 'scp/' . $item['href'];
 
@@ -19,7 +21,8 @@ if($nav && ($tabs=$nav->getTabs()) && is_array($tabs)){
                     '<li><a class="%s" href="%s" title="%s" id="%s">%s</a></li>',
                     $item['iconclass'],
                     $item['href'], $item['title'] ?? null,
-                    $id ?? null, $item['desc']);
+                    $id ?? null, $item['desc']
+                );
             }
             echo "\n</ul>\n";
         }
