cd72e2ec7e87cbf002c965f32300575c8880f56a
NasosBoras <borasnasos@gmail.com>
2023-01-18 23:16:47 +0200

    Made login and pwreset responsive.

--- include/client/pwreset.request.php
+++ include/client/pwreset.request.php
@@ -1,25 +1,36 @@
 <?php
-if(!defined('OSTCLIENTINC')) die('Access Denied');
+if (!defined('OSTCLIENTINC'))
+    die('Access Denied');
 
-$userid=Format::input($_POST['userid']);
+$userid = Format::input($_POST['userid']);
 ?>
-<h1><?php echo __('Forgot My Password'); ?></h1>
-<p><?php echo __(
-'Enter your username or email address in the form below and press the <strong>Send Email</strong> button to have a password reset link sent to your email account on file.');
-?>
+<h1>
+    <?php echo __('Forgot My Password'); ?>
+</h1>
+<p>
+    <?php echo __(
+        'Enter your username or email address in the form below and press the <strong>Send Email</strong> button to have a password reset link sent to your email account on file.'
+    );
+    ?>
 
-<form action="pwreset.php" method="post" id="clientLogin">
-    <div style="width:50%;display:inline-block">
-    <?php csrf_token(); ?>
-    <input type="hidden" name="do" value="sendmail"/>
-    <strong><?php echo Format::htmlchars($banner); ?></strong>
-    <br>
-    <div>
-        <label for="username"><?php echo __('Username'); ?>:</label>
-        <input id="username" type="text" name="userid" size="30" value="<?php echo $userid; ?>">
+<form action="pwreset.php" method="post" id="clientLogin" class="row">
+    <div class="col-12 col-sm-8">
+        <?php csrf_token(); ?>
+        <input type="hidden" name="do" value="sendmail" />
+        <strong><?php echo Format::htmlchars($banner); ?></strong>
+        <br>
+        <div>
+            <label for="username">
+                <?php echo __('Username'); ?>:
+            </label>
+            <input class="form-control" id="username" type="text" name="userid" size="30"
+                value="<?php echo $userid; ?>">
+        </div>
+        <p>
+            <input class="btn btn-outline-orange" type="submit" value="<?php echo __('Send Email'); ?>">
+        </p>
     </div>
-    <p>
-        <input class="btn" type="submit" value="<?php echo __('Send Email'); ?>">
-    </p>
+    <div class="lock-icon col-12 col-sm-4">
+        <img class="lock-icon" src="assets/default/images/lock.png?1319655200" alt="">
     </div>
 </form>
