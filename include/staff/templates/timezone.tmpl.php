69e24b5c5e3c5480f42ffb7ddb802acaa7bf97a3
NasosBoras <borasnasos@gmail.com>
2023-01-19 21:43:48 +0200

    Made register page responsive.

--- include/staff/templates/timezone.tmpl.php
+++ include/staff/templates/timezone.tmpl.php
@@ -4,21 +4,22 @@ $TZ_ALLOW_DEFAULT = isset($TZ_ALLOW_DEFAULT) ? $TZ_ALLOW_DEFAULT : true;
 $TZ_PLACEHOLDER = @$TZ_PLACEHOLDER ?: __('System Default');
 $TZ_TIMEZONE = @$TZ_TIMEZONE ?: '';
 ?>
-<select name="<?php echo $TZ_NAME; ?>" id="timezone-dropdown"
-        data-placeholder="<?php echo $TZ_PLACEHOLDER; ?>">
-<?php if ($TZ_ALLOW_DEFAULT) { ?>
+<select class="form-select" name="<?php echo $TZ_NAME; ?>" id="timezone-dropdown"
+    data-placeholder="<?php echo $TZ_PLACEHOLDER; ?>">
+    <?php if ($TZ_ALLOW_DEFAULT) { ?>
         <option value=""></option>
-<?php }
+    <?php }
     foreach (DateTimeZone::listIdentifiers() as $zone) { ?>
         <option value="<?php echo $zone; ?>" <?php
-        if ($TZ_TIMEZONE == $zone)
-            echo 'selected="selected"';
-        ?>><?php echo str_replace('/',' / ',$zone); ?></option>
-<?php } ?>
-    </select>
-    <button type="button" class="action-button" onclick="javascript:
+           if ($TZ_TIMEZONE == $zone)
+               echo 'selected="selected"';
+           ?>><?php echo str_replace('/', ' / ', $zone); ?>
+        </option>
+    <?php } ?>
+</select>
+<button type="button" class="action-button btn btn-secondary" onclick="javascript:
 $('head').append($('<script>').attr('src', '<?php
-    echo ROOT_PATH; ?>js/jstz.min.js'));
+echo ROOT_PATH; ?>js/jstz.min.js'));
 var recheck = setInterval(function() {
     if (window.jstz !== undefined) {
         clearInterval(recheck);
@@ -27,13 +28,15 @@ var recheck = setInterval(function() {
 
     }
 }, 100);
-return false;" style="vertical-align:middle"><i class="icon-map-marker"></i> <?php echo __('Auto Detect'); ?></button>
+return false;" style="vertical-align:middle"><i class="icon-map-marker"></i>
+    <?php echo __('Auto Detect'); ?>
+</button>
 
-<script type="text/javascript">
-$(function() {
-    $('#timezone-dropdown').select2({
-        allowClear: <?php echo $TZ_ALLOW_DEFAULT ? 'true' : 'false'; ?>,
-        width: '300px'
+<!-- <script type="text/javascript">
+    $(function () {
+        $('#timezone-dropdown').select2({
+            allowClear: <?php echo $TZ_ALLOW_DEFAULT ? 'true' : 'false'; ?>,
+            width: '500px'
     });
 });
-</script>
+</script> -->
